7284f87c08c5df51a9ad418630832ca64dc535b5
Андрей Зуев <andrej.zuev.2918@mail.ru>
2024-09-05 15:43:12 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+string inputFilePath = @"C:\Users\zueva\Desktop\data2.txt";
+
+try
+{
+    string content = File.ReadAllText(inputFilePath);
+    WriteToOutputFileIds(content, @"width: 63px;"">(\d+)</td>");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+void WriteToOutputFileIds(string content, string pattern)
+{
+    MatchCollection matches = Regex.Matches(content, pattern, RegexOptions.Singleline);
+
+    using (StreamWriter outputFile = new StreamWriter("OutputIds.txt"))
+    {
+        foreach (Match match in matches)
+        {
+            outputFile.WriteLine(match.Groups[1].Value);
+        }
+    }
+}
